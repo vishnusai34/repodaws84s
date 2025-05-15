e3fd38c08ce819e590294a2750c80b8d73c006cf
vishnusai34 <vishnu.karimisetty@gmail.com>
2025-05-15 15:28:18 +0530

    updated script with variables

--- session-01.sh
+++ session-01.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+
+PERSON1=PERSON2
+PERSON2=John
+
+
 echo "Hello this is my first shell script for practice"
+echo "$PERSON1:: Hey john what is the update"
+echo "$PERSON2:: Hi PERSON2 I am working on it"
+echo "$PERSON1:: Will it be completed by EOD"
+echo "$PERSON2:: will get finished $PERSON2"
+
+
+
 
 
 # add the code here
