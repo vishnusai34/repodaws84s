ad5bc984dcef55c53f05a898f48c52e78d1b432c
vishnusai34 <vishnu.karimisetty@gmail.com>
2025-05-15 15:40:22 +0530

    created new file and updated variables using values

---
+++ 04 variables.sh
@@ -0,0 +1,11 @@
+!/bin/bash
+
+PERSON1=$1 #there should be no spaces inbetween variables
+PERSON2=$2
+
+
+echo "Hello this is my first shell script for practice"
+echo "$PERSON1:: Hey john what is the update"
+echo "$PERSON2:: Hi PERSON2 I am working on it"
+echo "$PERSON1:: Will it be completed by EOD?"
+echo "$PERSON2:: will get finished $PERSON2"
