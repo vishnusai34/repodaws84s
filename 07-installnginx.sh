206610e50750e2f7dec09fdea3f007775c2b779c
vishnusai34 <vishnu.karimisetty@gmail.com>
2025-05-21 00:30:08 +0530

    .

---
+++ 07-installnginx.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+USERID-=$(id -u)
+
+if[ $USERID -nq 0]
+then 
+   echo " ERROR: user doesnot have root access Please access via root user"
+   exit 1
+else
+  echo "you are running with root access"
+fi
+
+dnf install nginx -y
+# $? - here it is command used to check the status of exit code status  whether it is success or failure sucess = 1 and failure 0-127 
+if[ $? -eq 0]
+then
+   echo "nginx is installed .... SUCCESS"
+else
+  echo "Nginx is not installed "
+  exit 1
+fi     
