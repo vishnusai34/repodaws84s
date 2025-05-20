1ffab8196327c34af41f22ba7648bcca84d5eb14
vishnusai34 <vishnu.karimisetty@gmail.com>
2025-05-21 00:43:14 +0530

    .

--- 07-installnginx.sh
+++ 07-installnginx.sh
@@ -2,15 +2,15 @@
 
 USERID=$(id -u)
 
-if [ $USERID -ne 0 ]
+if [ $USERID -ne 0 ] # spacing is required between the brackets and if
 then 
-    echo "ERROR: user doesnot have root access Please access via root user"
+    echo "ERROR: user doesnot have root access Please access via root user"  #indentation is must **********
     exit 1
 else
     echo "you are running with root access"
 fi
 
-dnf install nginx -y
+dnf install nginxcewcdc -y
 # $? - here it is command used to check the status of exit code status  whether it is success or failure sucess = 1 and failure 0-127 
 if [ $? -eq 0 ]
 then
