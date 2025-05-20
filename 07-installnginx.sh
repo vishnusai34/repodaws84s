3743b269bcb672d286d90331cce416cd7220c5a6
vishnusai34 <vishnu.karimisetty@gmail.com>
2025-05-21 00:56:56 +0530

    .

--- 07-installnginx.sh
+++ 07-installnginx.sh
@@ -10,12 +10,19 @@ else
     echo "you are running with root access"
 fi
 
-dnf install nginxcewcdc -y
-# $? - here it is command used to check the status of exit code status  whether it is success or failure sucess = 1 and failure 0-127 
-if [ $? -eq 0 ]
+dnf list installed nginx
+
+if [$? -ne 0 ]
 then
+    echo "Nginx is not installed ............going to install"
+    dnf install  nginx -y
+# $? - here it is command used to check the status of exit code status  whether it is success or failure sucess = 1 and failure 0-127 
+    if [ $? -eq 0 ]
+    then
     echo "nginx is installed .... SUCCESS"
+    else
+        echo "Nginx is not installed ..... FAILURE "
+        exit 1
 else
-    echo "Nginx is not installed ..... FAILURE "
-    exit 1
+    echo "Nginx is already installed ..... nothing is needed to install"        
 fi     
