789eadd68467228b05f3b201f5a1c2482ef771bc
vishnusai34 <vishnu.karimisetty@gmail.com>
2025-05-21 04:00:47 +0530

    .

--- 10-logs.sh
+++ 10-logs.sh
@@ -11,24 +11,24 @@ SCRIPT_NAME="$(echo $0 | cut -d "." -f1)"
 LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOG_FOLDER
-echo "Script started executing at: $(date)" 
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
 
 if [ $USERID -ne 0 ] # spacing is required between the brackets and if
 then 
-    echo -e "$R ERROR: user doesnot have root access Please access via root user $N" &>>$LOG_FILE #indentation is must **********
+    echo -e "$R ERROR: user doesnot have root access Please access via root user $N" | tee -a $LOG_FILE #indentation is must **********
     exit 1
 else
-    echo -e "$G you are running with root access $N" &>>$LOG_FILE
+    echo -e "$G you are running with root access $N" | tee -a $LOG_FILE
 fi
 
 # validate functions takes input as exit status, what command they tried to install
 VALIDATE(){ 
     if [ $1 -eq 0 ]
     then
-    echo -e "$2 is installed .... $G SUCCESS $N" &>>$LOG_FILE
+    echo -e "$2 is installed .... $G SUCCESS $N" | tee -a $LOG_FILE
     else
-        echo -e "$2 is not installed ..... $R FAILURE $N" &>>$LOG_FILE
+        echo -e "$2 is not installed ..... $R FAILURE $N" | tee -a $LOG_FILE
         exit 1
     fi 
 }
@@ -37,12 +37,12 @@ dnf list installed nginx &>>$LOG_FILE
 
 if [ $? -ne 0 ]
 then
-    echo "Nginx is not installed ............going to install" &>>$LOG_FILE
+    echo "Nginx is not installed ............going to install" | tee -a $LOG_FILE
     dnf install nginx -y &>>$LOG_FILE
     VALIDATE $? "nginx"  # here arg1 is exit status, arg2 is the installation application
 # $? - here it is command used to check the status of exit code status  whether it is success or failure sucess = 1 and failure 0-127   
 else
-    echo -e "Nginx is already installed ..... $Y nothing is needed to install $N"   &>>$LOG_FILE     
+    echo -e "Nginx is already installed ..... $Y nothing is needed to install $N" | tee -a $LOG_FILE     
 fi     
 
 
@@ -50,10 +50,10 @@ dnf list installed python3 &>>$LOG_FILE
 
 if [ $? -ne 0 ]
 then
-    echo "python3 is not installed ............going to install" &>>$LOG_FILE
+    echo "python3 is not installed ............going to install" | tee -a $LOG_FILE
     dnf install python3 -y &>>$LOG_FILE
     VALIDATE $? "python3" 
 # $? - here it is command used to check the status of exit code status  whether it is success or failure sucess = 1 and failure 0-127    
 else
-    echo -e "python3 is already installed ..... $Y nothing is needed to install $N" &>>$LOG_FILE      
+    echo -e "python3 is already installed ..... $Y nothing is needed to install $N" | tee -a $LOG_FILE      
 fi    
