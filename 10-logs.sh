05485ff84e552bd5514d981a54e30de798650a5a
vishnusai34 <vishnu.karimisetty@gmail.com>
2025-05-21 03:47:58 +0530

    .

--- 10-logs.sh
+++ 10-logs.sh
@@ -8,7 +8,7 @@ Y="\e[33m"
 N="\e[0m"
 LOG_FOLDER="/var/log/shell_script_logs"
 SCRIPT_NAME="$(echo $0 | cut -d "." -f1)"
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
 
 if [ $USERID -ne 0 ] # spacing is required between the brackets and if
 then 
